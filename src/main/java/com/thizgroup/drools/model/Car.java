d9cd3d434676e030d296158b0aaea950e96a2f57
hgq0916 <1286908118@qq.com>
2019-09-09 09:00:06 +0800

    简单规则

---
+++ src/main/java/com/thizgroup/drools/model/Car.java
@@ -0,0 +1,12 @@
+package com.thizgroup.drools.model;
+
+import lombok.Data;
+
+@Data
+public class Car {
+
+  private Person person;
+
+  private int discount;//折扣
+
+}
---
+++ src/main/java/com/thizgroup/drools/model/Person.java
@@ -0,0 +1,11 @@
+package com.thizgroup.drools.model;
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+  private Car car;
+
+  private int age;
+}
